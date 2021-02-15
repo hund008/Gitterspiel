3cea6c45e7af74ff8022fa5ee4f00afbbcbfbe71
Koglin <Koglin@DESKTOP-IMEF2NO.Speedport_W_724V_09011603_06_007>
2021-02-15 16:56:09 +0100

    added kannstmichmalPanel

---
+++ src/MalPanel.java
@@ -0,0 +1,32 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class MalPanel extends JPanel {
+	GitterSpiel frame;
+
+	public MalPanel(GitterSpiel frame) {
+		this.frame = frame;
+		GitterSpiel.inizcell();
+	}
+
+	public void paintComponent(Graphics g) {
+		for (int i = 0; i < 60; i++) {
+			g.drawLine(20, 20 + i * 20, 1200, 20 + i * 20);
+			g.drawLine(20 + i * 20, 20, 20 + i * 20, 850);
+		}
+		for (int i = 0; i < 61; i++) {
+			for (int j = 0; j < 61; j++) {
+				if (GitterSpiel.cell[i][j] == 1)
+					g.fillRect(i * 20, j * 20, 20, 20);
+			}
+		}
+		g.drawOval(frame.px, frame.py, 20, 20);
+		g.fillOval(frame.px, frame.py, 20, 20);
+//		g.setColor(Color.red);
+//		g.fillOval(KI.dx, KI.dy, 20, 20);
+		g.setColor(Color.black);
+
+	}
+}
